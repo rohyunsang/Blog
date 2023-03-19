75e099375c7ab265c86b8da972e718c6d8d77e9c
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-03-19 15:46:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,68 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html');
-    html = fs.readFileSync('network.html');
+const port = process.env.PORT || 3000;
+const http = require('http');
+const fs = require('fs');
+const formidable = require('formidable');
+const AWS = require('aws-sdk');
+const html = fs.readFileSync('index.html');
 
-var log = function(entry) {
+const log = function(entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
 };
 
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
+// AWS credentials
+AWS.config.update({
+  accessKeyId: 'YOUR_ACCESS_KEY_ID',
+  secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
+  region: 'ap-northeast-2'
+});
 
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
+const s3 = new AWS.S3();
 
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
+const uploadFile = function(file) {
+  const fileContent = fs.readFileSync(file.path);
+  const params = {
+    Bucket: 'YOUR_BUCKET_NAME',
+    Key: file.name,
+    Body: fileContent
+  };
+  s3.upload(params, function(err, data) {
+    if (err) {
+      console.log("Error uploading file: ", err);
     } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
+      console.log("File uploaded successfully. Location:", data.Location);
     }
+  });
+};
+
+const server = http.createServer(function (req, res) {
+  if (req.method === 'POST') {
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields, files) {
+      if (err) {
+        console.error('Error parsing form data', err);
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Internal server error');
+        return;
+      }
+
+      // Handle file upload
+      if (files.upload) {
+        console.log('File uploaded:', files.upload.name);
+        uploadFile(files.upload);
+      }
+
+      res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+      res.end();
+    });
+  } else {
+    res.writeHead(200);
+    res.write(html);
+    res.end();
+  }
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
 server.listen(port);
 
 // Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
